fcdafe2f8b6c5ad7543eedeb5243af43521ab400
Brian Christopher <108933205+brianchristy@users.noreply.github.com>
2025-06-26 23:18:56 +0530

    Update sessionManager.js

--- frontend/src/utils/sessionManager.js
+++ frontend/src/utils/sessionManager.js
@@ -1,9 +1,10 @@
 import { useAuthStore } from '../store/authStore';
 
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/auth"
-    : "/api/auth";
+const API_URL = "https://scan-backend-64s8.onrender.com";
+// const API_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5000/api/auth"
+//     : "/api/auth";
 
 class SessionManager {
   constructor() {
