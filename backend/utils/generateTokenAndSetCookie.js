c9b5a7b7c150744f0b7bb3c7d22b77829985cdab
brianchristy <brianchristopher170804@gmail.com>
2025-06-27 11:10:04 +0530

    admin panel routing

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -24,7 +24,6 @@ export const generateTokenAndSetCookie = async (res, userId) => {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
 		sameSite: "none",
-		path: "/",
 		maxAge: 15 * 60 * 1000, // 15 minutes
 	});
 
@@ -33,7 +32,6 @@ export const generateTokenAndSetCookie = async (res, userId) => {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
 		sameSite: "none",
-		path: "/",
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 
